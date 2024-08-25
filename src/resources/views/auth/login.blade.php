c735951fe199979f97b74fe1385a132a3f175b33
M0r1H <cmp.m0r1@icloud.com>
2024-08-25 23:04:09 +0900

    login&register.blade setting method

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -13,18 +13,19 @@
 
     <div class="form">
         <form action="/login" method="POST" class="login_form">
+            @csrf
             <div class="top_form"><b>Login</b></div>
                 <div class="login_inner_form">
                     <div class="contents_item">
                         <!-- メールアドレス入力欄 -->
                         <div class="adjust">
                             <label class="material-icons image" for="txtEmail">email</label>
-                            <input class="txt" type="text" id="txtEmail" placeholder="Email">
+                            <input class="txt" type="email" value="{{ old('email') }}" id="txtEmail" placeholder="Email"/>
                         </div>
                         <!-- パスワード入力欄 -->
                         <div class="adjust">
                             <label class="material-icons image" for="txtPassWord" >lock</label>
-                            <input class="txt" type="text" id="txtPassWord" placeholder="PassWord">
+                            <input class="txt" type="password" value="{{ old('password') }}" id="txtPassWord" placeholder="PassWord"/>
                         </div>
                     </div>
                     <button class="button_login">ログイン</button>
--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -12,24 +12,25 @@
 
 <div class="main">
     <div class="form">
-        <form action="" class="registration_form">
+        <form action="/register" method="POST" class="registration_form">
+            @csrf
             <div class="top_form"><b>Registration</b></div>
                 <div class="registration_inner_form">
                     <div class="contents_item">
                         <!-- ユーザー入力欄 -->
                         <div class="adjust">
                             <label class="material-icons image" for="txtUsername">person_4</label>
-                            <input class="txt" type="text" id="txtUsername" placeholder="UserName">
+                            <input class="txt" type="text" name="name" value="{{ old('name') }}" id="txtUsername"  placeholder="UserName"/>
                         </div>
                         <!-- メールアドレス入力欄 -->
                         <div class="adjust">
                             <label class="material-icons image" for="txtEmail">email</label>
-                            <input class="txt" type="text" id="txtEmail" placeholder="Email">
+                            <input class="txt" type="email" name="email" value="{{ old('email') }}" id="txtEmail" placeholder="Email"/>
                         </div>
                         <!-- パスワード入力欄 -->
                         <div class="adjust">
-                            <label class="material-icons image" for="txtPassWord" >lock</label>
-                            <input class="txt" type="text" id="txtPassWord" placeholder="PassWord">
+                            <label for="txtPassWord" class="material-icons image">lock</label>
+                            <input class="txt" type="password" name="password" value="{{ old('password') }}" id="txtPassWord" placeholder="PassWord"/>
                         </div>
                         <button type="submit" class="button_registration">登録</button>
                     </div>
